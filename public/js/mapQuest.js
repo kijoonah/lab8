7e2562ee94838191f553186fdf779e86a2488cde
Paul Kim <pkk001@ucsd.edu>
2020-02-27 10:51:56 -0800

    thirdpartyAPI lab

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,13 @@
 function initMap() {
-	// add your code here
-}
+
+	L.mapquest.key = 'IvKLe6d5VVyxpFkXjyBcGUbIrfgawkgm';
+	const map = L.mapquest.map('map', {
+	  center: [32.8788033, -117.2381008],
+	  layers: L.mapquest.tileLayer('map'),
+	  zoom: 12
+	  zoomControl: false
+	});
+
+  	L.marker([32.8788033, -117.2381008]).addTo(map);
+
+}  
--- routes/rsvp.js
+++ routes/rsvp.js
@@ -7,4 +7,10 @@ var data = {
  */
 exports.adminView = function(request, response){
   response.render('rsvp', data);
+  exports.addRSVP = function(request, response) {
+  var rsvpEmail = request.body.rsvpEmail;
+  console.log(rsvpEmail);
+
+  data.rsvp.push(rsvpEmail);
+  response.send(rsvpEmail);
 };
